fdda605291c926c875493cfaf2b2271a7885806f
Sambhav Shretha <sambhavshrestha1@gmail.com>
2026-05-17 14:25:56 -0400

    Define Raft Node struct

--- src/raft.rs
+++ src/raft.rs
@@ -1,3 +1,5 @@
+use tokio::sync::mpsc;
+
 #[derive(Debug)]
 enum NodeState {
 	Follower,
@@ -11,4 +13,14 @@ enum Message {
 	RequestVoteReply { from: u64, term: u64, granted: bool},
 	AppendEntries { from: u64, term: u64},
 	AppendEntriesReply { from: u64, term: u64, success: bool},
+}
+
+#[derive(Debug)]
+struct Node {
+	id: u64,
+	state: NodeState,
+	current_term: u64,
+	voted_for: Option<u64>,
+	votes_received: u64,
+	rx: mpsc::Receiver<Message>,
 }
